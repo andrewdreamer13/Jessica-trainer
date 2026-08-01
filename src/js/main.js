189379a95c05ff355875573d35d8bbc2c12bbbbc
Andrew Dreamer <andrewdreamer666@gmail.com>
2026-08-01 17:26:25 +0300

    clean up main.js

--- src/js/main.js
+++ src/js/main.js
@@ -8,46 +8,25 @@ import { Navigation, Pagination } from "swiper/modules";
 import "swiper/css";
 import "swiper/css/navigation";
 
-// import { optionsData } from "./data/selectOptions.js";
 import { phoneMasks } from "./data/phoneMasks.js";
 
 import { initPreloader } from "./components/preloader.js";
-// import { initBurger } from "./components/burger.js";
 import { initModal } from "./components/modalManager.js";
-// import { initLazySvg } from "./services/lazySvgLoader.js";
-// import { initAccordion } from "./components/accordion.js";
 import { initFocusManager } from "./services/focusManager.js";
-// import {initUpButton} from "./components/upButton/js";
 import { initSliders, testimonialsSwiper } from "./components/sliders.js";
-// import { initTabs } from "./components/tabs.js";
 import { initCookieBanner } from "./components/cookieBanner.js";
-// import { initCustomSelect } from "./components/customSelect.js";
 import { initFormHandler } from "./forms/formHandler.js";
-// import { initResizableSwiper } from "./services/matchMediaSlider.js";
-// import { initMaps } from "./services/lazyMapLoader.js";
-// import { initVideoLoader } from "./services/lazyVideoLoader.js";
 import { splitTextIntoSpans } from "./services/splitText.js";
 import { initExpandableText } from "./services/expandableText.js";
 
 document.addEventListener("DOMContentLoaded", () => {
   initPreloader();
-  // initBurger("#burger", ".nav", ".nav__list");
   initSliders();
   initModal();
-  // initAppearance();
-  // initAccordion("#faq");
   initFocusManager();
-  // initTabs("#tabs-1");
   initCookieBanner();
-  // initCustomSelect("#cities", optionsData.cities);
-  // initCustomSelect("#countries", optionsData.countries);
   initFormHandler("#contact-form");
-  // initResizableSwiper();
-  // initMaps();
-  // initVideoLoader();
   splitTextIntoSpans(".header__title");
-  // initUpButton(".footer__up-button");
-  // initExpandableText("[data-expandable]");
   initExpandableText("[data-expandable]", {
     onToggle: () => {
       if (!testimonialsSwiper) return;
@@ -55,14 +34,4 @@ document.addEventListener("DOMContentLoaded", () => {
       testimonialsSwiper.updateAutoHeight(300);
     },
   });
-
-  // (async () => {
-  //   try {
-  //     await import("./services/svgTemplates.js");
-  //     initLazySvg();
-  //   } catch (error) {
-  //     console.error("SVG template lazy-loading error:", error);
-  //     initLazySvg();
-  //   }
-  // })();
 });
